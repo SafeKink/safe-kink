47e91fd6174d0392dcf0f78fd7cd34454373ce4e
Eliot Rodriguez <mr.ear84@gmail.com>
2018-05-14 13:47:42 -0400

    updating work to include get to populate drop down menu based on database

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,10 +1,47 @@
 var express = require('express');
 var router = express.Router();
 var methodOverride = require('method-override');
-var kink = require('../models/kink.js');
+var state = require("../models/state");
 
+
+
+router.get("/", function(req, res) {
+    res.render("index");
+});
+
+
+router.get("/sandbox", function(req, res) {
+  state.all(function(data) {
+    var hbsObject = {
+      states: data
+    }; 
+
+    //var info = [{name: "shanti"}, {name: "shantero"}];
+
+    var states = hbsObject.states
+    //console.log(hbsObject.states[0].name);
+   // console.log(JSON.stringify(hbsObject));
+    //res.render("index");
+    res.render("sandbox", {states: states});
+  });
+});
+
+
+/*
 router.get('/', function(req,res){
+	function result(res){
+		var show = res;
+		console.log("from the result function: " + show);
+	}
+
+	var theStates = state.all(result());
+	console.log("States: " + theStates);
 		res.render('index');
+});
+*/
+
+router.get('/sandbox', function(req,res){
+	res.render('sandbox', {layout: 'sandboxL' });
 });
 
 
