9c3169d5b6fcbf0cff48746d4b4947a5469c777e
Eliot <mr.ear84@gmail.com>
2018-05-15 21:22:19 -0400

    routing to front end done

--- controllers/routes.js
+++ controllers/routes.js
@@ -2,8 +2,7 @@ var express = require('express');
 var router = express.Router();
 var methodOverride = require('method-override');
 var state = require("../models/state");
-
-
+var stiRate = require("../models/stiRate");
 
 
 router.get("/", function(req, res) {
@@ -18,19 +17,29 @@ router.get("/sandbox", function(req, res) {
       states: data
     };
     var states = info.states;
-    console.log(states);
+ //   console.log(states);
     res.render("sandbox", {states: states, layout: 'sandboxL'} );
   });
 });
 
 
-//2. When user picks the state
+//2. When user picks the states
+/*
 router.get("/api/states/:statePicked", function(req, res){
+	console.log("call the route");
+	//grab the user selected state id and put it in a stateID 
+	var stateID = {"states.id": req.params.statePicked};
 
- console.log("ok you are here now" + req.params.statePicked);
- res.redirect('/sandbox');
-
+	//send the info for processing to the database
+	stiRate.joinSelect(stateID, function(data){
+		//SEND THE RETRIEVED DATA TO THE FRONT END
+	res.json(data);
+	});
 });
+*/
+
+	//joinSelect: function (condition, cb){
+ //console.log("ok you are here now" + req.params.statePicked);
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var exphbs = require('express-handlebars');
 var routes = require('./controllers/routes.js');
+var stiRate = require("./models/stiRate");
+
 
 var app = express();
 
@@ -31,6 +33,22 @@ app.engine('handlebars', exphbs({
 }));
 
 app.set('view engine', 'handlebars');
+
+
+	app.get("/api/states/:statePicked", function(req, res){
+		console.log("this works");
+
+		var stateID = {"states.id": req.params.statePicked};
+	//send the info for processing to the database
+	stiRate.joinSelect(stateID, function(data){
+		//SEND THE RETRIEVED DATA TO THE FRONT END
+		console.log(data);
+	res.json(data);
+	});
+		//res.json(friendsData);
+});
+
+
 
 var PORT = 3000;
 app.listen(PORT, function(){
