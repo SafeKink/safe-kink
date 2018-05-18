d0ae39b298038794d6593a17e4f7fcc4e0a1ccee
Eliot Rodriguez <mr.ear84@gmail.com>
2018-05-18 11:52:15 -0400

    updating port access info

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.set('view engine', 'handlebars');
 
 
 
-var PORT = 3000;
+var PORT = process.env.PORT || 8080;
 app.listen(PORT, function(){
 	console.log("Server listening on: http://localhost:" + PORT);
 }); 
